95c1c093a6a5d0d5718dbbbf088530dc5ce623f5
etjnyc <Eddie@shulmanpaper.com>
2026-08-12 23:22:35 -0400

    install production board transport

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ShulmanRestaurantPortalV2 from "./ShulmanRestaurantPortalV2";
+import { installProductionBoardTransport } from "./productionBoardTransport";
 import "./index.css";
+
+installProductionBoardTransport();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
